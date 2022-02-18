448ee857af2d94dd082484054dd6b2ff18ccef04
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-18 13:02:48 -0700

    wrote searchPage

--- src/SearchPage.js
+++ src/SearchPage.js
@@ -1,7 +1,28 @@
-import React from 'react';
+import React, { useState } from 'react';
+import MovieList from './MovieList';
+import { searchMovies } from './services/fetch-utils';
 
 export default function SearchPage() {
+  const [search, setSearch] = useState('');
+  const [results, setResults] = useState([]);
+  const [watchlist, setWatchlist] = useState([]);
+
+  async function handleSearch(e) {
+    e.preventDefault();
+
+    const movies = await searchMovies(search);
+    setResults(movies);
+  }
+
   return (
-    <div>SearchPage</div>
+    <div>SearchPage
+      <form onSubmit={handleSearch}>
+        <input value={search} onChange={e => setSearch(e.target.value)} />
+        <button>Search</button>
+      </form>
+      <section>Results:
+        <MovieList movies={results} />
+      </section>
+    </div>
   );
 }
