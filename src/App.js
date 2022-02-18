4aa23370720bcc82e8f7b6bca7ac4573c8c5361f
Sam-Melius <91688565+Sam-Melius@users.noreply.github.com>
2022-02-18 11:37:19 -0700

    wrote App.js

--- src/App.js
+++ src/App.js
@@ -1,24 +1,82 @@
-import logo from './logo.svg';
+import {
+  BrowserRouter as Router,
+  Route,
+  Switch,
+  Redirect,
+  NavLink
+} from './react-router-dom';
 import './App.css';
+import { useEffect, useState } from 'react';
+import { getUser, logout } from './services/fetch-utils';
+import HomePage from './HomePage';
+import SearchPage from './SearchPage';
+import WatchList from './WatchList';
+
 
 function App() {
+  const [user, setUser] = useState('');
+
+  useEffect(() => {
+    async function fetch() {
+      const user = await getUser();
+      if (user) setUser(user);
+    }
+    fetch();
+  });
+
+  async function handleLogout() {
+    await logout();
+    setUser('');
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Router>
+      <div className="App">
+        <header>
+          { user && 
+            <ul>
+              <li>
+                <NavLink to='search'>SearchPage</NavLink>
+              </li>
+              <li>
+                <NavLink to='/watchlist'>Watchlist</NavLink>
+              </li>
+              <li>
+                <button onClick={handleLogout}>Logout</button>
+              </li>
+            </ul>
+          }
+        </header>
+        <main>
+          <Switch>
+            <Route exact path='/'>
+              {
+                user
+                  ? <Redirect to='/search'/>
+                  : <HomePage setUser={setUser}/>
+              }
+            </Route>
+            <Route exact path='/search'>
+              {
+                !user
+                  ? <Redirect to='/'/>
+                  : <SearchPage />
+              }
+            </Route>
+            <Route exact path='/watchlist'>
+              {
+                !user
+                  ? <Redirect to='/'/>
+                  : <WatchList />
+              }
+            </Route>
+
+          </Switch>
+        </main>
+      
+      </div>
+    </Router>
+    
   );
 }
 
